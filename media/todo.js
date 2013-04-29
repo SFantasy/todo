173dec4c2388afe5013b93a74131c70a204b5293
SFantasy <forever.fantasy27@gmail.com>
2013-04-29 18:18:58 +0800

    'Add some styles'

--- media/todo.js
+++ media/todo.js
@@ -1,12 +1,30 @@
 var $ = function(id) { return document.getElementById(id) };
 
-var todo = {
+var Todo = {
+	init: function() {
+		this.newTodo();
+	},
+
 	newTodo: function() {
 		$('new').onkeydown = function(e) {
-			e = e || window.event;
-			if(e.keyCode === 13) {
-				console.log("Yes!");
+			e = e || this.event;
+			if(e.keyCode === 13 && this.value) {
+				// add new todo to the list
+				var newItem = document.createElement('li');
+				newItem.innerHTML = this.value;
+				$('list').appendChild(newItem);
+				this.value = '';
+				Todo.updateCount();
+			} else {
+				return;
 			}
 		};
+	},
+
+	updateCount: function() {
+		$('count').firstChild.innerHTML = 
+			$('list').getElementsByTagName('li').length.toString();
 	}
 }
+
+Todo.init();
