29147eb30e47bbe163c5868baaf9ef8ede679389
SFantasy <forever.fantasy27@gmail.com>
2013-08-20 00:02:26 +0800

    Build up your backbone

--- todo.js
+++ todo.js
@@ -1,8 +1,24 @@
-var Todo = {
+var Todo = function() {
+	this.todoList = {}
+}
+
+$.extend(Todo.prototype, {
 	init: function() {
+		Todo.bindEvent()
 	},
 	bindEvent: function() {
+		$('#add').on('click', function() {
+			Todo.newTodo()
+		})
+		$.each($('#list'), function(i, item) {
+
+		})
 	},
 	newTodo: function() {
+		var content = $('#content').val()
+		$('#list').prepend('<li>' + content + 
+			'<button class="complete">complete</button><button class="remove">remove</button></li>' )
 	}
-}
+})
+
+Todo.init();
